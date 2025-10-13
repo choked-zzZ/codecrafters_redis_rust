edc28c05dc1d5f09913536a4a6e32c6b6dbf4d29
choked-zzZ <azureskyemusic@gmail.com>
2025-10-13 22:27:45 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -122,11 +122,14 @@ async fn replica_handler(addr: String, args: &Arc<Args>, env: Arc<Mutex<Env>>) {
                     let value = Arc::new(value);
                     let command = RedisCommand::parse_command(value.clone());
                     let response = command.clone().exec(env.clone(), addr, args.clone()).await;
-                    eprintln!("{addr} will send back: {response:?}");
-                    if let Err(e) = framed.send(&response).await {
-                        eprintln!("carsh into error: {e}");
-                        eprintln!("connection closed.");
-                        return;
+                    if !command.can_modify() {
+
+                        // eprintln!("{addr} will send back: {response:?}");
+                        // if let Err(e) = framed.send(&response).await {
+                        //     eprintln!("carsh into error: {e}");
+                        //     eprintln!("connection closed.");
+                        //     return;
+                        // }
                     }
                     if matches!(command, RedisCommand::PSync(..)) {
                         break;
